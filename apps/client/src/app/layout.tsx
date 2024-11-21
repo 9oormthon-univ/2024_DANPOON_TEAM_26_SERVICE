3f008337567d9a49bf801c794c9d7e4166e960a8
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-11-21 20:39:22 +0900

    [chore] font 설정 (#24)
    * chore(client): font 설정
    
    * chore(client): lint 스크립트 추가

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -6,15 +6,54 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const sfPro = localFont({
+  src: [
+    {
+      path: "./fonts/SF-Pro-Display-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "./fonts/SF-Pro-Display-Heavy.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "./fonts/SF-Pro-Display-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/SF-Pro-Display-Semibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/SF-Pro-Display-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/SF-Pro-Display-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/SF-Pro-Display-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./fonts/SF-Pro-Display-Thin.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./fonts/SF-Pro-Display-Ultralight.woff2",
+      weight: "100",
+      style: "normal",
+    },
+  ],
 });
 
 export const metadata: Metadata = {
@@ -29,7 +68,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${sfPro.className} antialiased`}>
         <Provider>
           <FullWidthAndCenter>
             <DefaultHeader />
