f12cc2d895d436f0741446371e61c9f9ccf8e50e
Hyomin Koo <me@eatsteak.dev>
2024-11-24 08:02:30 +0900

    fix(gasi): don't panic if agi req failed (#82)

--- apps/gasi/src/agi.ts
+++ apps/gasi/src/agi.ts
@@ -1,16 +1,21 @@
+import { TRPCError } from "@trpc/server";
 import { server } from "./index.js";
 
 const AGI_IP = process.env.AGI_IP;
 
 export const requestGeneration = async (userId: string) => {
   server.log.info(`[REQUEST/AGI] Request generation to ${AGI_IP} - ${userId}`);
-  return await fetch(`http://${AGI_IP}/api/assignment`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      id: userId,
-    }),
-  });
+  try {
+    return await fetch(`http://${AGI_IP}/api/assignment`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: userId,
+      }),
+    });
+  } catch (e) {
+    throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: "AGI 요청에 실패했습니다." });
+  }
 };
